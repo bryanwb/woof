4c09d07a2b312f65ad53155f9aab3ddc8a0f5d19
hitman <hitman@hiroko.(none)>
2012-10-21 21:16:28 +0200

    parse values from composite JMX objects

--- bin/woof.rb
+++ bin/woof.rb
@@ -1,85 +1,101 @@
-# require 'choice'
-# require 'jmx'
-# require 'pry'
+require 'choice'
+require 'jmx'
+require 'pry'
 
-# jmx_queries = {
-#   {
-#     :result_alias => "ajp-thread-pool",
-#     :name => "Catalina:type=ThreadPool,name=\"ajp-bio-8009\"",
-#     :attrs => [
-#                "maxHeaderCount",
-#                "maxThreads",
-#                "connectionCount",
-#                "currentThreadCount",
-#                "currentThreadsBusy"
-#               ]
-#   },
-#   {
-#     :result_alias => "http-thread-pool",
-#     :name => "Catalina:type=ThreadPool,name=\"http-bio-8080\""
-#     :attrs => [
-#                "maxHeaderCount",
-#                "maxThreads",
-#                "connectionCount",
-#                "currentThreadCount",
-#                "currentThreadsBusy"
-#               ]
-#   },
-#   {
-#     :result_alias => "jvm-threads",
-#     :name => "java.lang:type=Threading",
-#     :attrs => [
-#                "DaemonThreadCount",
-#                "PeakThreadCount",
-#                "ThreadCount",
-#                "TotalStartedThreadCount"
-#               ]
-#   },
-#   {
-#     :alias => "memory",
-#     :name => "java.lang:type=Memory",
-#     :attrs => [ "HeapMemoryUsage", "NonHeapMemoryUsage" ]
-#   },
-#   {
-#     :alias => "memorypool",
-#     :name => "java.lang:type=MemoryPool,name=*",
-#     :attrs => [ "Usage" ]
-#   },
-#   {
-#     :alias => "gc",
-#     :name => "java.lang:type=GarbageCollector,name=*",
-#     :attrs => [ "CollectionCount", "CollectionTime" ]
-#   },
-#   {
-#     :name => "Catalina:type=ThreadPool,name=*",
-#     :alias => "connectors",
-#     :attrs => [ "currentThreadCount", "currentThreadsBusy", "" ]
-#   },
-#   {
-#     :name => "Catalina:type=GlobalRequestProcessor,name=*",
-#     :alias => "requests",
-#     :attrs => [ "bytesReceived", "bytesSent", "errorCount", "maxTime", "processingTime", "requestCount" ]
-#   },
-#   {  :name => "Catalina:type=DataSource,class=javax.sql.DataSource,name=*",
-#     :alias  => "datasources",
-#     :attrs => [ "NumActive", "NumIdle", "NumQueryThreads" ]
-#   }
+jmx_queries = [
+               {
+                 :alias => "ajp-thread-pool",
+                 :name => "Catalina:type=ThreadPool,name=\"ajp-bio-8009\"",
+                 :attrs => [
+                            "maxHeaderCount",
+                            "maxThreads",
+                            "connectionCount",
+                            "currentThreadCount",
+                            "currentThreadsBusy"
+                           ]
+               },
+               {
+                 :alias => "http-thread-pool",
+                 :name => "Catalina:type=ThreadPool,name=\"http-bio-8080\"",
+                 :attrs => [
+                            "maxHeaderCount",
+                            "maxThreads",
+                            "connectionCount",
+                            "currentThreadCount",
+                            "currentThreadsBusy"
+                           ]
+               },
+               {
+                 :alias => "jvm-threads",
+                 :name => "java.lang:type=Threading",
+                 :attrs => [
+                            "DaemonThreadCount",
+                            "PeakThreadCount",
+                            "ThreadCount",
+                            "TotalStartedThreadCount"
+                           ]
+               },
+               {
+                 :alias => "memory",
+                 :name => "java.lang:type=Memory",
+                 :attrs => [ "HeapMemoryUsage", "NonHeapMemoryUsage" ]
+               },
+               {
+                 :alias => "memorypool",
+                 :name => "java.lang:type=MemoryPool,name=*",
+                 :attrs => [ "Usage" ]
+               },
+               {
+                 :alias => "gc",
+                 :name => "java.lang:type=GarbageCollector,name=*",
+                 :attrs => [ "CollectionCount", "CollectionTime" ]
+               },
+               {
+                 :name => "Catalina:type=ThreadPool,name=*",
+                 :alias => "connectors",
+                 :attrs => [ "currentThreadCount", "currentThreadsBusy"  ]
+               },
+               {
+                 :name => "Catalina:type=GlobalRequestProcessor,name=*",
+                 :alias => "requests",
+                 :attrs => [ "bytesReceived", "bytesSent", "errorCount", "maxTime", "processingTime", "requestCount" ]
+               },
+               {
+                 :name => "Catalina:type=DataSource,class=javax.sql.DataSource,name=*",
+                 :alias  => "datasources",
+                 :attrs => [ "NumActive", "NumIdle", "NumQueryThreads" ]
+               }
+              ]
 
-# }
 
-# client = JMX.connect(:port => 3000)
+client = JMX.connect(:port => 3000)
 
-# jmx_objs.each do |q|
-#   client.query_names(q[:name]).flatten!.each do |jmx_obj|
-#     q.attrs.each do |attr|
-#       puts q[:alias] + " " + client.server.get_attribute(jmx_obj, attr.to_java_string)
-#     end
-#   end
-# end
-
-while true
-  sleep 1
+def get_jmx_objs(name, client)
+  client.query_names(name).entries
 end
+
+def get_jmx_attributes(jmx_obj, attr, client)
+  attrs = client.server.get_attribute(jmx_obj, attr.to_java_string)
+  if attrs.class == Java::JavaxManagementOpenmbean::CompositeDataSupport
+    attrs.map{|e| [e,attrs[e]] }
+  else
+    [[attr, attrs]]
+  end
+end
+
+
+jmx_queries.each do |q|
+  get_jmx_objs(q[:name],client).each do |jmx_obj|
+    q[:attrs].each do |attr|
+      jmx_vals = get_jmx_attributes(jmx_obj, attr, client)
+      jmx_vals.each do |k,v|
+        puts "#{q[:alias]}-#{attr}-#{k}-#{v.to_s}"
+      end
+    end
+  end
+end
+
+
 
 
 
